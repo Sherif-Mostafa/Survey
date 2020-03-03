d524aa49544e7df38d7df4d1c28e18383f5ef8f7
sherif-mohamed3 <Sherif.Mohamed3@vodafone.com>
2020-03-03 02:00:59 +0200

    init app

---
+++ backend/routes/users.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+let jwt = require('jsonwebtoken');
+
+global.config = require('./config');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+ /* Post users Login. */
+ router.post('/login', function (req, res, next) {
+  let userdata = {
+  username: req.body.username,
+  password: req.body.password
+  };
+
+  console.log(req)
+  
+  //Go to server for user varificarion
+  if (userdata.username == "shashangka" && userdata.password == "12345") {
+    let token = jwt.sign(userdata, global.config.secretKey, {
+      algorithm: global.config.algorithm,
+      expiresIn: '1m'
+      });
+     
+  res.status(200).json({
+    message: 'Login Successful',
+    jwt: token
+    });
+  }
+  else {
+  res.status(401).json({
+  message: 'Login Failed'
+  });
+  }
+  });
+ 
+
+module.exports = router;
